8ec86c084f7bd73fe8562b5d3a9057d918787432
Tiranya <illangamudalitiranya@gmail.com>
2024-12-24 22:50:11 +0000

    Update question 2 (fibonacci sequence).R
    created the fibonacci sequence and filtered out the even numbers

--- RStudio/project Euler/question 2 (fibonacci sequence).R
+++ RStudio/project Euler/question 2 (fibonacci sequence).R
@@ -33,3 +33,28 @@ while (n3<80)
 x=c(x,n3)  
 }
 print (x)
+
+#step 2 - filtering out the even numbers. 
+if (x%%2==0)
+{y=c(y,x)}  
+print (y)
+
+#step2 - attempt 2
+y=0
+if (x%%2==0)
+{y=c(y,x)}  
+
+#attempt 3
+y=0
+if(x%%2==0)
+{y=c(y,x)}  
+
+#cannot use a series of vectors in a if has to use a loop
+#attempt4
+y=c()
+for (n in x) 
+if (n%%2==0)  
+{y=c(y,n)}  
+print (y)
+
+
