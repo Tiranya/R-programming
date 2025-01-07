f478eb5407cea378ad9c27c8f7de1f0b37133f37
Tiranya <illangamudalitiranya@gmail.com>
2025-01-07 22:01:43 +0000

    Data Analysis using R
    I started the Youtube course, 'Data Analysis using R', by Danny Arends

---
+++ from Danny Arends lectures and assignments/lecture 1- initialising/Assignment1.R
@@ -0,0 +1,41 @@
+setwd("D:\programming\languages\R-programming\R-programming\from Danny Arends lectures and assignments\lecture 1- initialising")
+#Data Analysis Using R by Danny Arends 
+#Lecture 1 - Assignment 1
+#copyright (c) 2025 - Tiranya Illanga Mudali
+#Date written: 07/01/2025
+#
+#Using R as a calculator
+1234+4567
+#output = [1] 5801
+#
+100456-3350+23
+#output = [1] 97129
+#
+#natural logarithm of 15
+log(15)
+#output = [1] 2.70805
+#
+4596/12
+#output = [1] 383
+#
+#Euclidean division - division of one integer with another
+#Euclidean division remainder of 10 and 6
+10%%6
+#output = [1] 4
+#
+#Euclidean division answer without decimals
+10%/%6
+#output = [1] 1
+#
+#normal answer
+10/6
+#output = [1] 1.666667
+#
+sqrt (-8)
+#output = [1] NaN
+#'NaN' - this means = 'not a number'
+#
+sqrt (4)
+#output - [1] 2
+#
+sqrt (8)
